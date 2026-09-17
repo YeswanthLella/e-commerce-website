9b6912ab428b430193495d351d9c517c4c74b971
cinemood18 <cinemood18@gmail.com>
2026-09-17 15:03:56 +0530

    fetch data from backend

--- ecommerce proj/src/pages/Homepage.jsx
+++ ecommerce proj/src/pages/Homepage.jsx
@@ -1,8 +1,15 @@
 import { Header } from '../Components/Header';
 import './Homepage.css'
 import {products} from '../../Starting-code//data/products'
-export function Homepage()
-{
+export function Homepage(){
+fetch('http://localhost:3000/api/products')
+  .then((response)=>{
+        return response.json();
+  }).then((data)=>{
+          console.log(data);
+        });
+
+
     return( 
         <>
   <Header />
